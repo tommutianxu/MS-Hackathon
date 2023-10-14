bddac826d5d29f1cff902bf2fb6d4248bb41da91
tommutianxu <mutianxu101@gmail.com>
2023-10-14 07:55:09 +0100

    Test commit 2

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # Testing setting up environment and push/pull from github
-
-
+import os
 os.getcwd()
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import QuantLib as ql
+
+print("Test1")
+
+
